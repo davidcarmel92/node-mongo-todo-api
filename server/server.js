b3fe6a2ec2e1fc91e77456eb0980327bb86d4489
David <davidcarmel@Davids-MacBook-Pro.local>
2018-08-03 07:29:51 -0400

    commited test-servers

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -14,12 +15,51 @@ app.post('/todos', (req, res) => {
     text: req.body.text
   })
   todo.save().then((doc) => {
-    res.send(doc)
+    res.send(doc);
   }, (e) => {
-    res.status(400).send(e)
+    res.status(400).send(e);
+  });
+});
+
+app.post('/user', (req, res) => {
+  const user = new User({
+    email: req.body.text
+  })
+  user.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos', (req,res) => {
+  Todo.find().then((todos) => {
+    res.send({todos})
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req,res) => {
+  const id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send()
+    }
+    res.send({todo})
+  }).catch((e) => {
+    res.status(400).send(e);
   });
 });
 
 app.listen(3000, () => {
   console.log('App on port 3000')
 })
+
+module.exports = {
+  app
+}
