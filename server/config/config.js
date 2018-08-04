5556d94a96125fbc52e3c4222b696345d17ae828
David <davidcarmel@Davids-MacBook-Pro.local>
2018-08-04 19:43:53 -0400

    heroku mlab

--- server/config/config.js
+++ server/config/config.js
@@ -7,3 +7,6 @@ if(env === 'development') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
 }
+else {
+  process.env.MONGODB_URI = 'mongodb://dcarmel92:bonnie77@ds113522.mlab.com:13522/todoapp'
+}
